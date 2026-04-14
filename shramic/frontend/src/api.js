d701159085053556005b55c21360ad84abcd1155
samruddhiarabhavi <samruddhiarabhavi@gmail.com>
2026-04-14 19:36:12 +0530

    vercel

--- shramic/frontend/src/api.js
+++ shramic/frontend/src/api.js
@@ -1,23 +1,5 @@
-import axios from 'axios'
-
-const api = axios.create({ baseURL: '/api' })
-
-api.interceptors.request.use(cfg => {
-  const token = localStorage.getItem('shramic_token')
-  if (token) cfg.headers.Authorization = `Bearer ${token}`
-  return cfg
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : '/api'
 })
-
-api.interceptors.response.use(
-  r => r,
-  err => {
-    if (err.response?.status === 401) {
-      localStorage.removeItem('shramic_token')
-      localStorage.removeItem('shramic_user')
-      window.location.href = '/login'
-    }
-    return Promise.reject(err)
-  }
-)
-
-export default api
